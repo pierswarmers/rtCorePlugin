af652fd61bb907a3ae842056aac045db5a496cf0
Piers Warmers <hello@pierswarmers.com>
2010-03-02 13:51:37 +1100

    Added support for abstract object routinging link.

--- modules/gnSearch/templates/indexSuccess.php
+++ modules/gnSearch/templates/indexSuccess.php
@@ -13,8 +13,12 @@
 <?php if(isset($pager)): ?>
 <div class="gn-search-results">
   <p><?php echo count($pager) . ' ' . __('results found.') ?></p>
-  <?php foreach($pager->getResults() as $gn_index): ?>
-  <p><?php echo $gn_index->getModel() ?> <?php echo $gn_index->getModelId() ?> -- <strong><?php echo $gn_index->getObject()->getTitle() ?></strong></p>
-  <?php endforeach; ?>
+  <?php if(count($pager) > 0): ?>
+    <ul>
+    <?php foreach($pager->getResults() as $gn_index): ?>
+      <li><?php echo link_to($gn_index->getObject()->getTitle(), Doctrine_Inflector::tableize($gn_index->getCleanModel()).'_show', $gn_index->getObject()) ?></li>
+    <?php endforeach; ?>
+    </ul>
+  <?php endif; ?>
 </div>
 <?php endif; ?>

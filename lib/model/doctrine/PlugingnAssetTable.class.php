dd8888a51c818232e22ca3b3385937ff7ce968fb
Piers Warmers <hello@pierswarmers.com>
2010-03-08 12:54:02 +1100

    Added some abstraction between object vs model and model_id handling.

--- lib/model/doctrine/PlugingnAssetTable.class.php
+++ lib/model/doctrine/PlugingnAssetTable.class.php
@@ -3,17 +3,50 @@
  */
 class PlugingnAssetTable extends Doctrine_Table
 {
-  public function getAssetsForObject($model, $model_id, Doctrine_Query $q = null)
+  /**
+   * Return assets for a given record object.
+   *
+   * @param Doctrine_Record $object
+   * @param Doctrine_Query $q
+   * @return Doctrine_Collection
+   * @see PlugingnAssetTable::getAssetsForModelAndId()
+   */
+  public function getAssetsForObject(Doctrine_Record $object, Doctrine_Query $q = null)
   {
-    $q = $this->getQuery($q);
+    return $this->getAssetsForModelAndId(get_class($object), $object->getId(), $q);
+  }
 
-    $q->andWhere('a.model = ?', $model)
-      ->andWhere('a.model_id = ?', $model_id)
-      ->orderBy('a.position, a.id');
-
+  /**
+   * Return assets for a given record class and id.
+   * 
+   * @param string $model
+   * @param string $model_id
+   * @param Doctrine_Query $q
+   * @return Doctrine_Collection
+   */
+  public function getAssetsForModelAndId($model, $model_id, Doctrine_Query $q = null)
+  {
+    $q = $this->getQueryForModelAndId($model, $model_id, $q);
+    $q->orderBy('a.position');
     return $q->execute();
   }
 
+  /**
+   * Add the model and model_id query components.
+   * 
+   * @param string $model
+   * @param string $model_id
+   * @param Doctrine_Query $q
+   * @return Doctrine_Query
+   */
+  public function getQueryForModelAndId($model, $model_id, Doctrine_Query $q = null)
+  {
+    $q = $this->getQuery($q);
+    $q->andWhere('a.model = ?', $model)
+      ->andWhere('a.model_id = ?', $model_id);
+    return $q;
+  }
+  
   /**
    * Returns a Doctrine_Query object.
    *

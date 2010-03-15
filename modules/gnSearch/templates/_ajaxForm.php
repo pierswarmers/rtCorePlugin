93de312d2f5a8b7b0f2798e5cf7948bb4bcfdd22
Piers Warmers <hello@pierswarmers.com>
2010-03-16 00:31:49 +1100

    Update to a cleaner seperation for the linking helper.

--- modules/gnSearch/templates/_ajaxForm.php
+++ modules/gnSearch/templates/_ajaxForm.php
@@ -1,13 +1,12 @@
 <?php $rand = rand(); ?>
-<div class="gn-modal-panel" id="gnLinkPanel">
+<a href="#" id="gnLinkPanelTrigger<?php $rand = rand(); ?>" rel="#gnLinkPanel<?php $rand = rand(); ?>">Insert Link</a>
+<div class="gn-modal-panel" id="gnLinkPanel<?php $rand = rand(); ?>">
   <h2><?php echo __('Find a page to link to:') ?></h2>
   <div class="inner">
-    <form id="gnLinkForm<?php echo $rand ?>" class="gn-search-form" action="<?php echo url_for('@gn_search_ajax?sf_format=json') ?>" method="get">
-      <p>
-        <input type="text" name="q" id="gnLinkFormQuery<?php echo $rand ?>" value="" />
-        <button type="submit" class="button"><?php echo __('Search') ?></button>
-      </p>
-    </form>
+    <p>
+      <input type="text" name="q" id="gnLinkPanelQuery<?php echo $rand ?>" value="" />
+      <button type="submit" class="button" id="gnLinkPanelId<?php echo $rand ?>"><?php echo __('Search') ?></button>
+    </p>
     <ul id="gnLinkPanelResults<?php echo $rand ?>" class="inner-panel">&nbsp;</ul>
   </div>
 </div>
@@ -19,23 +18,12 @@
         opacity: 0.4
       }
     });
-    $("#gnLinkForm<?php echo $rand ?>").submit(function() {
-      $.ajax({
-        url: '<?php echo url_for('@gn_search_ajax?sf_format=json') ?>',
-        dataType: 'json',
-        data: { q : $('#gnLinkFormQuery<?php echo $rand ?>').attr('value') },
-        success: function(data) {
-          $('#gnLinkPanelResults<?php echo $rand ?>').html('');
-          $.each(data.items, function(index, value) {
-            $('<li><a href="#" onclick="injectTextIntoID(\'['+value.title+']('+value.link+')\', \'gn_wiki_page_en_content\'); $(\'#gnLinkPanel\').close(); return false;">'+value.title+'</a></li>').appendTo('#gnLinkPanelResults<?php echo $rand ?>');
-          });
-        }
-      });
-      return false;
-    });
+    enableLinkPanel(
+      '#gnLinkPanelQuery<?php echo $rand ?>',
+      '#gnLinkPanelId<?php echo $rand ?>',
+      '#gnLinkPanelResults<?php echo $rand ?>',
+      '<?php echo url_for('@gn_search_ajax?sf_format=json') ?>',
+      '<?php echo $targetId ?>'
+    );
   });
 </script>
-
-
-
-
--- web/js/main.js
+++ web/js/main.js
@@ -3,12 +3,38 @@
  *
  * http://www.scottklarr.com
  */
-/*
- * Based on code by Scott Klarr
- *
- * http://www.scottklarr.com
- */
 function injectTextIntoCurrent(text) { injectTextIntoID(text, $("*:focus").attr('id')); }
-
-
 function injectTextIntoID(text, itemId) { var txtarea = document.getElementById(itemId); if(txtarea == null) { return false; }; var scrollPos = txtarea.scrollTop; var strPos = 0; var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ? "ff" : (document.selection ? "ie" : false ) ); if (br == "ie") { txtarea.focus(); var range = document.selection.createRange(); range.moveStart ('character', -txtarea.value.length); strPos = range.text.length; } else if (br == "ff") strPos = txtarea.selectionStart; var front = (txtarea.value).substring(0,strPos); var back = (txtarea.value).substring(strPos,txtarea.value.length); txtarea.value=front+text+back; strPos = strPos + text.length; if (br == "ie") { txtarea.focus(); var range = document.selection.createRange(); range.moveStart ('character', -txtarea.value.length); range.moveStart ('character', strPos); range.moveEnd ('character', 0); range.select(); } else if (br == "ff") { txtarea.selectionStart = strPos; txtarea.selectionEnd = strPos; txtarea.focus(); } txtarea.scrollTop = scrollPos; }
+
+/*
+ * Enable the link panel.
+ */
+function enableLinkPanel(inputId, buttonId, updatePanel, updateUrl, targetTextField)
+{
+  $(inputId).keydown(function(event) {
+    if (event.keyCode == '13') {
+      triggerLinkPanelLookup(inputId, updatePanel, updateUrl, targetTextField);
+      event.preventDefault();
+    }
+  });
+  $(buttonId).click(function() {
+    triggerLinkPanelLookup(inputId, updatePanel, updateUrl, targetTextField);
+  });
+}
+/*
+ * Trigger the search / lookup for the link panel
+ */
+function triggerLinkPanelLookup(inputId, updatePanel, updateUrl, targetTextField)
+{
+  $.ajax({
+    url: updateUrl,
+    dataType: 'json',
+    data: {q : $(inputId).attr('value')},
+    success: function(data) {
+      $(updatePanel).html('');
+      $.each(data.items, function(index, value) {
+        $('<li><a href="#" onclick="injectTextIntoID(\'['+value.title+']('+value.link+')\', \''+targetTextField+'\'); return false;">'+value.title+'</a></li>').appendTo(updatePanel);
+      });
+    }
+  });
+}

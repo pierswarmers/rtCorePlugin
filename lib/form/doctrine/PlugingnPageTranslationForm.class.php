8bd84521a4d320eef3d1c85d0b7c2e70bd5f426d
Piers Warmers <hello@pierswarmers.com>
2010-03-19 12:36:02 +1100

    Added reference to new widget.

--- lib/form/doctrine/PlugingnPageTranslationForm.class.php
+++ lib/form/doctrine/PlugingnPageTranslationForm.class.php
@@ -21,13 +21,21 @@ abstract class PlugingnPageTranslationForm extends BasegnPageTranslationForm
   {
     parent::setup();
     unset($this['version'], $this['created_at'], $this['updated_at']);
-    $this->setWidget('title',      new sfWidgetFormInputText(array(), array('class' => 'title')));
-    $this->setWidget('tags',       new sfWidgetFormInput(array(), array('class' => 'tag-input')));
+
+    // set the widgets
+    $this->setWidget('title',       new sfWidgetFormInputText(array(), array('class' => 'title')));
+    $this->setWidget('content',     new gnWidgetFormTextareaMarkdown(array(), array()));
+    $this->setWidget('tags',        new sfWidgetFormInput(array(), array('class' => 'tag-input')));
+    $this->setWidget('description', new sfWidgetFormInputText(array(), array()));
+
+    // inject the tags into the default value
     $this->setDefault('tags', implode(', ', $this->getObject()->getTags()));
-    $this->setValidator('tags',    new sfValidatorString(array('required' => false)));
-    $this->setValidator('title',   new sfValidatorString(array('max_length' => 255, 'required' => true), array('required' => 'please enter a descriptive title.')));
-    $this->setValidator('content', new sfValidatorString(array('required' => true), array('required' => 'please enter some content.')));
-    $this->setWidget('description',      new sfWidgetFormInputText(array(), array()));
+
+    // set the validators
+    $this->setValidator('tags',     new sfValidatorString(array('required' => false)));
+    $this->setValidator('title',    new sfValidatorString(array('max_length' => 255, 'required' => true), array('required' => 'please enter a descriptive title.')));
+    $this->setValidator('content',  new sfValidatorString(array('required' => true), array('required' => 'please enter some content.')));
+
     $this->widgetSchema->setHelp('description', 'As short description describing this page.');
   }
 }

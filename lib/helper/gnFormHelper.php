a0138e49408b9033e8e788ecabf9839122af7157
Piers Warmers <hello@pierswarmers.com>
2010-03-22 12:21:38 +1100

    Changed the style and format of form rows.

--- lib/helper/gnFormHelper.php
+++ lib/helper/gnFormHelper.php
@@ -46,41 +46,34 @@ function render_form_row(sfFormField $widget, $options = array())
   
   $is_checkbox = get_class($widget->getWidget()) === 'sfWidgetFormInputCheckbox';
 
-  $label = $widget->renderLabel();
+  $html = '';
 
-  if(!isset($options['format']))
+  if($is_checkbox)
   {
-    if($is_checkbox)
-    {
-      $options['format'] = '<div class="%1$s checkbox"><label>%4$s %6$s %2$s</label> %3$s %5$s</div>';
-      $label = $widget->renderLabelName();
-    }
-    else
-    {
-      $options['format'] = '<div class="%1$s standard">%2$s %6$s %3$s %4$s %5$s</div>';
-    }
+    $html = sprintf(
+      '<div class="%1$s checkbox"><label>%4$s %6$s %2$s</label> %3$s %5$s</div>',
+      $options['class'], // 1
+       $widget->renderLabelName(),  // 2
+      $widget->hasError() ? ' &rarr; '. $widget->renderError() : '',  // 3
+      $widget->render(), // 4
+      $widget->renderHelp(),  // 5
+      $content// 6
+    );
+  }
+  else
+  {
+    $html = sprintf(
+      '<div class="%s standard"><label for="%s">%s <span class="error">%s</span></label> %s %s %s</div>',
+      $options['class'],
+      $widget->renderId(),
+      $widget->renderLabelName(),
+      $widget->hasError() ? ' &rarr; '. $widget->renderError() : '',
+      $widget->render(),
+      $widget->renderHelp(),
+      $content
+    );
   }
 
-  if($options['wide'] && !$is_checkbox)
-  {
-    $options['class'] .= ' gn-form-wide';
-  }
-
-  if($widget->hasError())
-  {
-    $options['class'] .= ' gn-error';
-  }
-
-  $html = sprintf(
-    $options['format'],
-    $options['class'], // 1
-    $label,  // 2
-    $widget->hasError() ? ' &rarr; '. $widget->renderError() : '',  // 3
-    $widget->render(), // 4
-    $widget->renderHelp(),  // 5
-          $content// 6
-  ) . "\n";
-
-  return $html;
+  return $html . "\n";
 }
 

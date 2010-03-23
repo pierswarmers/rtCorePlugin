c31c805307e12ac8bf210e38c614658b0b55ba8f
Piers Warmers <hello@pierswarmers.com>
2010-03-23 17:57:51 +1100

    Corrected a reference to bad variable.

--- modules/gnAsset/templates/_asset_row.php
+++ modules/gnAsset/templates/_asset_row.php
@@ -14,5 +14,5 @@
     <span><?php echo gnAssetToolkit::getFormattedBytes($asset->getFilesize()); ?></span>
   </div>
   <span class="delete" onclick="deleteAsset('<?php echo $asset->getId() ?>')">&times;</span>
-  <span class="insert" onclick="injectTextIntoCurrent('<?php echo $asset->isImage() ? '!' : '' ?>[<?php echo $description_text; ?>](asset:<?php echo $asset->getOriginalFilename() ?><?php echo $asset->isImage() ? '|right|200,300' : '' ?>)')">&nbsp;</span>
+  <span class="insert" onclick="injectTextIntoCurrent('<?php echo $asset->isImage() ? '!' : '' ?>[<?php echo __('Description goes here...'); ?>](asset:<?php echo $asset->getOriginalFilename() ?><?php echo $asset->isImage() ? '|right|200,300' : '' ?>)')">&nbsp;</span>
 </li>

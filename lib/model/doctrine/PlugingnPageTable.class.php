7c2bd5b3fac08e920c8ba98aeb33ddd624e50e91
Piers Warmers <hello@pierswarmers.com>
2010-03-24 00:36:20 +1100

    Adeed arrange of access methods for retrieving pages.

--- lib/model/doctrine/PlugingnPageTable.class.php
+++ lib/model/doctrine/PlugingnPageTable.class.php
@@ -17,5 +17,39 @@
  */
 class PlugingnPageTable extends Doctrine_Table
 {
+  public function findAllPublished(Doctrine_Query $query = null)
+  {
+    $query = $this->addPublishedQuery($query);
+    return $query->andWhere('page.deleted_at IS NULL')->execute();
+  }
 
+  public function findAllPages(Doctrine_Query $query = null)
+  {
+    return $this->getQuery($query)->andWhere('page.deleted_at IS NULL')->execute();
+  }
+
+  public function addPublishedQuery(Doctrine_Query $query = null)
+  {
+    $query = $this->getQuery($query);
+    $query->andWhere('(page.published_from < ? OR page.published_from IS NULL)', date('Y-m-d H:i:s', time()));
+    $query->andWhere('(page.published_to > ? OR page.published_to IS NULL)', date('Y-m-d H:i:s', time()));
+    $query->andWhere('page.published = 1');
+    return $query;
+  }
+
+  /**
+   * Return a query object, creting a new one if needed.
+   *
+   * @param Doctrine_Query $query
+   * @param string $alias
+   * @return Doctrine_Query
+   */
+  public function getQuery(Doctrine_Query $query = null)
+  {
+    if(is_null($query))
+    {
+      return parent::createQuery('page');
+    }
+    return $query;
+  }
 }

fd836147191eeae27860e14abf8150aeaf4ef0a6
Piers Warmers <hello@pierswarmers.com>
2010-05-28 15:37:32 +1000

    Corrected rt back to gn.

--- lib/helper/rtAnalyticsHelper.php
+++ lib/helper/rtAnalyticsHelper.php
@@ -43,7 +43,7 @@ function analytics($web_property_id = null)
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTartame('script')[0]; s.parentNode.insertBefore(ga, s);
+    var s = document.getElementsByTagname('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
 </script>
 

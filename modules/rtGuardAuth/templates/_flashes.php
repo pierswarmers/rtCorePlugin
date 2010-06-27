500de85a121ac2f4a643dc052733abb6cae14dde
Piers Warmers <hello@pierswarmers.com>
2010-06-28 09:31:24 +1000

    Removed unused partial.

--- modules/rtGuardAuth/templates/_flashes.php
+++
@@ -1,5 +0,0 @@
-<?php if ($sf_user->hasFlash('notice')): ?>
-  <p class="success"><?php echo $sf_user->getFlash('notice') ?></p>
-<?php elseif ($sf_user->hasFlash('error')): ?>
-  <p class="error"><?php echo $sf_user->getFlash('error') ?></p>
-<?php endif; ?>

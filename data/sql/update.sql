44d9bceca0fbcf59edcb9f7271104544c0fde780
Piers Warmers <hello@pierswarmers.com>
2010-06-30 10:55:58 +1000

    Added update to table aterations.

--- data/sql/update.sql
+++ data/sql/update.sql
@@ -5,3 +5,12 @@
 ALTER TABLE `rt_site` ADD COLUMN `reference_key` VARCHAR(255);
 ALTER TABLE `rt_site` DROP COLUMN `template_dir`;
 ALTER TABLE `rt_site` DROP COLUMN `is_primary`;
+
+-- Version 0.0.2 - June 29 2010
+
+ALTER TABLE `rt_address` ADD COLUMN `instructions` TEXT;
+ALTER TABLE `rt_address` ADD COLUMN `first_name` VARCHAR(100);
+ALTER TABLE `rt_address` ADD COLUMN `last_name` VARCHAR(100);
+
+ALTER TABLE `rt_shop_order` ADD COLUMN `voucher_code` VARCHAR(100);
+

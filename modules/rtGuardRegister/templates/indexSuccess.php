aa8dc71e7cf3ae9dffa8f5250753b10a567628f2
Piers Warmers <hello@pierswarmers.com>
2010-07-01 14:49:39 +1000

    Corrected references to missing flashes.php partial

--- modules/rtGuardRegister/templates/indexSuccess.php
+++ modules/rtGuardRegister/templates/indexSuccess.php
@@ -1,4 +1,4 @@
 <?php use_helper('I18N') ?>
 <h1><?php echo __('Register', null, 'sf_guard') ?></h1>
-<?php include_partial('rtGuardAuth/flashes')?>
+<?php include_partial('rtAdmin/flashes_public')?>
 <?php echo get_partial('rtGuardRegister/form', array('form' => $form)) ?>
--- modules/rtGuardRegister/templates/pendingSuccess.php
+++ modules/rtGuardRegister/templates/pendingSuccess.php
@@ -1,4 +1,4 @@
 <?php use_helper('I18N') ?>
 <h1><?php echo __('Registration Pending') ?></h1>
-<?php include_partial('rtGuardAuth/flashes')?>
+<?php include_partial('rtAdmin/flashes_public')?>
 <p><?php echo __('You have successfully registered and your account is pending activation. An email will be sent when the account has been activated.') ?></p>

b960416f512a4031289c942c01ec3023cc5e46df
Torsten Hirsch <torsten@wranglers.com.au>
2010-07-05 09:30:31 +1000

    add default_snippet_content

--- modules/rtSnippet/templates/_snippetPanel.php
+++ modules/rtSnippet/templates/_snippetPanel.php
@@ -1,9 +1,13 @@
 <?php use_helper('I18N', 'Date', 'rtText', 'rtForm', 'rtDate', 'rtSite') ?>
 <?php if ($snippets): ?>
-<?php foreach($snippets as $snippet): ?>
-<div class="rt-nippet rt-show rt-primary-container rt-admin-edit-tools-panel">
-  <?php echo link_to(__('Edit'), 'rtSnippetAdmin/edit?id='.$snippet->getId(), array('class' => 'rt-admin-edit-tools-trigger')) ?>
-  <?php echo markdown_to_html($snippet->getContent(), $snippet); ?>
-</div>
-<?php endforeach; ?>
+  <div class="rt-nippet rt-show rt-primary-container rt-admin-edit-tools-panel">
+    <?php if(count($snippets) >= 1): ?>
+      <?php foreach($snippets as $snippet): ?>
+        <?php echo link_to(__('Edit'), 'rtSnippetAdmin/edit?id='.$snippet->getId(), array('class' => 'rt-admin-edit-tools-trigger')) ?>
+        <?php echo markdown_to_html($snippet->getContent(), $snippet); ?>
+      <?php endforeach; ?>
+    <?php else: ?>
+      <?php echo markdown_to_html($default_snippet_content); ?>
+    <?php endif; ?>
+  </div>
 <?php endif; ?>

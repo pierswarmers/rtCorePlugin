5a96a0817d81bd1db5b141a7dcbfb8ce57d655b8
Piers Warmers <hello@pierswarmers.com>
2010-07-05 13:18:24 +1000

    Corrected flash partial reference.

--- modules/rtGuardForgotPassword/templates/changeSuccess.php
+++ modules/rtGuardForgotPassword/templates/changeSuccess.php
@@ -1,7 +1,7 @@
 <?php use_helper('I18N', 'rtForm') ?>
 <h1><?php echo __('Hello %name%', array('%name%' => $user->getName()), 'sf_guard') ?></h1>
 <p><?php echo __('Enter your new password in the form below.', null, 'sf_guard') ?></p>
-<?php include_partial('rtGuardAuth/flashes')?>
+<?php include_partial('rtAdmin/flashes_public')?>
 <form action="<?php echo url_for('@sf_guard_forgot_password_change?unique_key='.$sf_request->getParameter('unique_key')) ?>" method="POST">
   <?php echo $form->renderHiddenFields() ?>
   <p><?php echo $form['password']->renderLabel() ?> <?php echo $form['password'] ?></p>
--- modules/rtGuardForgotPassword/templates/indexSuccess.php
+++ modules/rtGuardForgotPassword/templates/indexSuccess.php
@@ -4,7 +4,7 @@
   <?php echo __('Do not worry, we can help you get back in to your account safely!', null, 'sf_guard') ?>
   <?php echo __('Fill out the form below to request an e-mail with information on how to reset your password.', null, 'sf_guard') ?>
 </p>
-<?php include_partial('rtGuardAuth/flashes')?>
+<?php include_partial('rtAdmin/flashes_public')?>
 <form action="<?php echo url_for('@sf_guard_forgot_password') ?>" method="post">
   <?php echo $form->renderHiddenFields() ?>
   <p><?php echo $form['email_address']->renderLabel() ?> <?php echo $form['email_address'] ?></p>

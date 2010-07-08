492bbfa29ca7629decb319b2043a9444f34594fc
Piers Warmers <hello@pierswarmers.com>
2010-07-08 14:40:07 +1000

    Corrected wording on password reset email.

--- modules/rtGuardForgotPassword/templates/_new_password.php
+++ modules/rtGuardForgotPassword/templates/_new_password.php
@@ -1,5 +1,5 @@
 <?php use_helper('I18N') ?>
 <?php echo __('Hi %first_name%', array('%first_name%' => $user->getFirstName()), 'sf_guard') ?>,
-<?php echo __('Below you will your username and new password:') ?> 
+<?php echo __('Below is your username and new password:') ?>
 <?php echo __('Username', null, 'sf_guard') ?>: <?php echo $user->getUsername() ?> 
 <?php echo __('Password', null, 'sf_guard') ?>: <?php echo $password ?>

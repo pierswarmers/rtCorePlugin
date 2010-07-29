27dfcd9ca0d28992ab8b42d39fded121735e611e
Piers Warmers <hello@pierswarmers.com>
2010-07-30 09:59:05 +1000

    Refactored the class to have better seperation and modularity.

--- modules/rtGuardUser/lib/BasertGuardUserActions.class.php
+++ modules/rtGuardUser/lib/BasertGuardUserActions.class.php
@@ -17,12 +17,20 @@
  */
 class BasertGuardUserActions extends sfActions
 {
+  /**
+   * Set the template.
+   */
   public function preExecute()
   {
     rtTemplateToolkit::setFrontendTemplateDir();
     parent::preExecute();
   }
-  
+
+  /**
+   * Run the edit screen screen.
+   * 
+   * @param sfWebRequest $request 
+   */
   public function executeEdit(sfWebRequest $request)
   {
     $id = $this->getUser()->getGuardUser()->getId();
@@ -30,18 +38,27 @@ class BasertGuardUserActions extends sfActions
     $this->form = new rtGuardUserPublicForm($rt_guard_user);
   }
 
+  /**
+   * Run update logic for a user.
+   * 
+   * @param sfWebRequest $request
+   */
   public function executeUpdate(sfWebRequest $request)
   {
     $id = $this->getUser()->getGuardUser()->getId();
     $this->forward404Unless($request->isMethod(sfRequest::POST));
     $this->forward404Unless($rt_guard_user = Doctrine::getTable('rtGuardUser')->find(array($id)), sprintf('Object rt_guard_user does not exist (%s).', $id));
     $this->form = new rtGuardUserPublicForm($rt_guard_user);
-
     $this->processForm($request, $this->form);
-
     $this->setTemplate('edit');
   }
 
+  /**
+   * Process the form with a set of request data.
+   * 
+   * @param sfWebRequest $request
+   * @param sfForm $form
+   */
   protected function processForm(sfWebRequest $request, sfForm $form)
   {
     $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
@@ -54,5 +71,16 @@ class BasertGuardUserActions extends sfActions
       $this->redirect('rt_guard_account');
     }
     $this->getUser()->setFlash('default_error', true, false);
+  }
+
+  /**
+   * Return a form object.
+   * 
+   * @param sfGuardUser $user
+   * @return rtGuardUserPublicForm
+   */
+  protected function getForm(sfGuardUser $user)
+  {
+    return new rtGuardUserPublicForm($rt_guard_user);
   }
 }

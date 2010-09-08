3fe333d89490c461f2f90ec6000a35bee7dd2376
SeaShadow <konny@wranglers.com.au>
2010-09-08 16:20:29 +1000

    Added sitemap module

---
+++ modules/rtSitemap/actions/actions.class.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * This file is part of the reditype package.
+ * (c) 2009-2010 Piers Warmers <piers@wranglers.com.au>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once(dirname(__FILE__).'/../lib/BasertSitemapActions.class.php');
+
+/**
+ * BasertSitemapActions
+ *
+ * @package    rtSitePlugin
+ * @subpackage modules
+ * @author     Piers Warmers <piers@wranglers.com.au>
+ * @author     Konny Zurcher <konny@wranglers.com.au>
+ */
+class rtSitemapActions extends BasertSitemapActions
+{
+
+}

ccb81101af4a899e37530e150f03183e1bad04af
SeaShadow <konny@wranglers.com.au>
2010-09-08 16:20:29 +1000

    Added sitemap module

---
+++ modules/rtSitemap/lib/BasertSitemapActions.class.php
@@ -0,0 +1,121 @@
+<?php
+
+/*
+ * This file is part of the gumnut package.
+ * (c) 2009-2010 Piers Warmers <piers@wranglers.com.au>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * BasertSitemapActions
+ *
+ * @package    rtSitemapPlugin
+ * @subpackage modules
+ * @author     Piers Warmers <piers@wranglers.com.au>
+ * @author     Konny Zurcher <konny@wranglers.com.au>
+ */
+class BasertSitemapActions extends sfActions
+{
+  /**
+   * Executes an application defined process prior to execution of this sfAction object.
+   *
+   * By default, this method is empty.
+   */
+  public function preExecute()
+  {
+    sfConfig::set('app_rt_node_title', 'Sitemap');
+    rtTemplateToolkit::setFrontendTemplateDir();
+  }
+
+  /**
+   * Executes index page and forwards to sitemap
+   *
+   * @param sfWebRequest $request
+   */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('rtSitemap', 'sitemap');
+  }
+
+  /**
+   * Executes sitemap. Decides by sf_format to supply html or xml output
+   *
+   * @param sfWebRequest $request
+   */
+  public function executeSitemap(sfWebRequest $request)
+  {
+    if($request->getParameter('sf_format') == 'xml')
+    {
+      $this->site_pages = $this->getSitePages();
+      $this->blog_pages = $this->getBlogPages();
+      $this->wiki_pages = $this->getWikiPages();
+      $this->shop_products = $this->getShopProducts();
+      $this->shop_categories = $this->getShopCategories();
+    }
+  }
+
+  /**
+   * Returns pages as array
+   *
+   * @return array pages
+   */
+  protected function getSitePages()
+  {
+    return $this->getQuery('rtSitePage');
+  }
+
+  /**
+   * Returns posts as array
+   *
+   * @return array posts
+   */
+  protected function getBlogPages()
+  {
+    return $this->getQuery('rtBlogPage');
+  }
+
+  /**
+   * Returns wiki pages as array
+   *
+   * @return array wiki pages
+   */
+  protected function getWikiPages()
+  {
+    return $this->getQuery('rtWikiPage');
+  }
+
+  /**
+   * Returns shop products as array
+   *
+   * @return array products
+   */
+  protected function getShopProducts()
+  {
+    return $this->getQuery('rtShopProduct');
+  }
+
+  /**
+   * Returns shop categories as array
+   *
+   * @return array categories
+   */
+  protected function getShopCategories()
+  {
+    return $this->getQuery('rtShopCategory');
+  }
+
+  /**
+   * Returns query for tablename supplied
+   *
+   * @return array
+   */
+  protected function getQuery($string)
+  {
+    $query = Doctrine::getTable($string)->addPublishedQuery();
+    $query = Doctrine::getTable($string)->addSiteQuery($query);
+    $query->andWhere('page.searchable = ?', true);
+    return $query->fetchArray();
+  }
+}

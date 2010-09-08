7a6438f639bd2ec4533268acece23a1d5ec692ec
SeaShadow <konny@wranglers.com.au>
2010-09-08 16:20:31 +1000

    Added sitemap module

---
+++ modules/rtSitemap/templates/sitemapSuccess.xml.php
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<?php $routes = $sf_context->getRouting()->getRoutes() ?>
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+
+<?php if(isset($routes['rt_site_page_index'])): ?>
+  <?php foreach($site_pages as $site_page): ?>
+  <url>
+    <loc><?php echo url_for('rt_site_page_show', array('id' => $site_page['id'], 'slug' => $site_page['slug']), true) ?></loc>
+    <changefreq>daily</changefreq>
+    <lastmod><?php echo $site_page['updated_at'] ?></lastmod>
+  </url>
+  <?php endforeach; ?>
+<?php endif; ?>
+
+<?php if(isset($routes['rt_blog_page_index'])): ?>
+  <?php foreach($blog_pages as $blog_page): ?>
+  <url>
+    <loc><?php echo url_for('rt_blog_page_show', array('id' => $blog_page['id'], 'slug' => $blog_page['slug']), true) ?></loc>
+    <changefreq>daily</changefreq>
+    <lastmod><?php echo $blog_page['updated_at'] ?></lastmod>
+  </url>
+  <?php endforeach; ?>
+<?php endif; ?>
+
+<?php if(isset($routes['rt_wiki_page_index'])): ?>
+  <?php foreach($wiki_pages as $_wiki_page): ?>
+  <url>
+    <loc><?php echo url_for('rt_wiki_page_show', array('id' => $_wiki_page['id'], 'slug' => $_wiki_page['slug']), true) ?></loc>
+    <changefreq>daily</changefreq>
+    <lastmod><?php echo $_wiki_page['updated_at'] ?></lastmod>
+  </url>
+  <?php endforeach; ?>
+<?php endif; ?>
+
+<?php if (isset($routes['rt_shop_product_show'])): ?>
+  <?php foreach($shop_products as $shop_product): ?>
+  <url>
+    <loc><?php echo url_for('rt_shop_product_show', array('id' => $shop_product['id'], 'slug' => $shop_product['slug']), true) ?></loc>
+    <changefreq>daily</changefreq>
+    <lastmod><?php echo $shop_product['updated_at'] ?></lastmod>
+  </url>
+  <?php endforeach; ?>
+<?php endif; ?>
+
+<?php if (isset($routes['rt_shop_category_index'])): ?>
+  <?php foreach($shop_categories as $shop_category): ?>
+  <url>
+    <loc><?php echo url_for('rt_shop_category_show', array('id' => $shop_category['id'], 'slug' => $shop_category['slug']), true) ?></loc>
+    <changefreq>daily</changefreq>
+    <lastmod><?php echo $shop_category['updated_at'] ?></lastmod>
+  </url>
+  <?php endforeach; ?>
+<?php endif; ?>
+
+</urlset>

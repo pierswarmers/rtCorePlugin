4a3dc63ef4be2f72da8690ff1dc381178801c7e5
Piers Warmers <hello@pierswarmers.com>
2010-09-16 13:55:14 +1000

    Cleaned the search form and intered new html5 placeohlder text

--- modules/rtSearch/templates/_form.php
+++ modules/rtSearch/templates/_form.php
@@ -1,8 +1,6 @@
-<div class="rt-search-holder">
-  <form class="rt-search-form" action="<?php echo url_for('@rt_search') ?>" method="get">
-    <p>
-      <input type="text" name="q" value="<?php echo $sf_request->getParameter('q', '') ?>" />
-      <button type="submit" class="button"><?php echo __('Search') ?></button>
-    </p>
-  </form>
-</div>
+<form class="rt-search" action="<?php echo url_for('@rt_search') ?>" method="get">
+  <p>
+    <input type="text" name="q" value="<?php echo $sf_request->getParameter('q', '') ?>" placeholder="<?php echo __('Enter your search terms') ?>..." />
+    <button type="submit" class="button"><?php echo __('Search') ?></button>
+  </p>
+</form>

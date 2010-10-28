f19d16944b8e9351c2bdc6e64609bee0b9facf04
Piers Warmers <hello@pierswarmers.com>
2010-10-29 09:07:48 +1100

    Ugraded handling of headings and div structure.

--- modules/rtSitemap/templates/sitemapSuccess.php
+++ modules/rtSitemap/templates/sitemapSuccess.php
@@ -1,11 +1,15 @@
 <?php use_helper('I18N', 'Date', 'rtText', 'rtForm', 'rtDate', 'rtSite'); ?>
 <?php $routes = $sf_context->getRouting()->getRoutes() ?>
 
-<h2><?php echo __('Sitemap') ?></h2>
+<?php slot('rt-title') ?>
+<?php echo __('Sitemap') ?>
+<?php end_slot(); ?>
+
+<h2><?php echo __('Site Pages') ?></h2>
 <?php include_component('rtSitePage', 'navigation', array('options' => array('render_full' => true))) ?>
 
 <?php if (isset($routes['rt_shop_category_index'])): ?>
-  <h2><?php echo __('Your Shop Categories') ?></h2>
+  <h2><?php echo __('Shop Categories') ?></h2>
   <?php include_component('rtShopCategory', 'navigation') ?>
 <?php endif; ?>
 

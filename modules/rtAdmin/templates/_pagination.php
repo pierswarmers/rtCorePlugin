92e70e2427394b7fc10fd3aa4d6f78ca85de0163
Piers Warmers <hello@pierswarmers.com>
2010-10-29 08:54:13 +1100

    Added some modifications to the classes used in the pagination.

--- modules/rtAdmin/templates/_pagination.php
+++ modules/rtAdmin/templates/_pagination.php
@@ -21,19 +21,19 @@ if($sf_request->hasParameter('show_more'))
 <?php if ($pager->haveToPaginate()): ?>
 <div class="rt-pager rt-container">
   <ul>
-    <li class="pager-first-page<?php echo $pager->getPage() == $pager->getFirstPage() ? ' here' : '' ?>">
+    <li class="rt-pager-first<?php echo $pager->getPage() == $pager->getFirstPage() ? ' here' : '' ?>">
       <a href="<?php echo '?page=1'.$params ?>"><?php echo $arrows_enabled ? $arrows_lft.$arrows_lft : '' ?> <?php echo __($titles_first) ?></a>
     </li>
-    <li class="pager-previous-page<?php echo $pager->getPage() == $pager->getFirstPage() ? ' here' : '' ?>">
+    <li class="rt-pager-previous<?php echo $pager->getPage() == $pager->getFirstPage() ? ' here' : '' ?>">
       <a href="<?php echo '?page='.$pager->getPreviousPage().$params ?>"><?php echo $arrows_enabled ? $arrows_lft : '' ?> <?php echo __($titles_previous) ?></a>
     </li>
-    <li class="pager-last-page<?php echo $pager->getPage() == $pager->getLastPage() ? ' here' : '' ?>">
+    <li class="rt-pager-last<?php echo $pager->getPage() == $pager->getLastPage() ? ' here' : '' ?>">
       <a href="<?php echo '?page='.$pager->getLastPage().$params ?>"><?php echo __($titles_last) ?> <?php echo $arrows_enabled ? $arrows_rgt.$arrows_rgt : '' ?></a>
     </li>
-    <li class="pager-next-page<?php echo $pager->getPage() == $pager->getLastPage() ? ' here' : '' ?>">
+    <li class="rt-pager-next<?php echo $pager->getPage() == $pager->getLastPage() ? ' here' : '' ?>">
       <a href="<?php echo '?page='.$pager->getNextPage().$params ?>"><?php echo __($titles_next) ?> <?php echo $arrows_enabled ? $arrows_rgt : '' ?></a>
     </li>
-    <li class="pages">
+    <li class="rt-pages">
       <ul>
         <?php
           foreach ($pager->getLinks() as $page):

97f892a2d94d37089a6f284def764dd77afbdc10
Piers Warmers <hello@pierswarmers.com>
2010-10-29 00:53:58 +1100

    Added further validation and help text to user details form.

--- lib/form/rtGuardUserPublicForm.class.php
+++ lib/form/rtGuardUserPublicForm.class.php
@@ -23,6 +23,38 @@ class rtGuardUserPublicForm extends rtGuardUserForm
       $this['groups_list'],
       $this['permissions_list']
     );
+
+
+    $this->widgetSchema->setHelp('first_name', 'Required');
+    $this->setValidator('first_name', new sfValidatorString(array('required' => true)));
+    $this->widgetSchema->setHelp('last_name', 'Required');
+    $this->setValidator('last_name', new sfValidatorString(array('required' => true)));
+
+
+    $this->widgetSchema->setHelp('email_address', 'Required');
+    $this->widgetSchema->setHelp('username', 'Required');
+
+
+    $this->widgetSchema->setHelp('password', 'Required - at least 6 characters long');
+    $this->setValidator('password', new sfValidatorString(array('required' => false, 'min_length' => 6)));
+    $this->widgetSchema->setHelp('password_again', 'Once again, just to be sure');
+
+
+    $this->widgetSchema->setHelp('url', 'Must start with: http:// or https://');
+    $this->setValidator('url', new sfValidatorUrl(array('required' => false), array('invalid' => 'Please enter a valid website address.')));
+
+    $this->widgetSchema->setLabel('url', 'Website');
+    $this->widgetSchema->setHelp('url', 'Must start with: http:// or https://');
+    $this->setValidator('url', new sfValidatorUrl(array('required' => false), array('invalid' => 'Please enter a valid website address.')));
+
+
+
+    $this->validatorSchema->setPostValidator(
+      new sfValidatorAnd(array(
+        new sfValidatorDoctrineUnique(array('model' => 'sfGuardUser', 'column' => array('email_address')), array('invalid' => 'That email address is already taken.')),
+        new sfValidatorDoctrineUnique(array('model' => 'sfGuardUser', 'column' => array('username')), array('invalid' => 'That username is already taken.')),
+      ))
+    );
   }
 }
 ?>

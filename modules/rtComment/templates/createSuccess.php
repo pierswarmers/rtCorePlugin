feeeb55f6e05347d70b4113f80a41887e82e0a3c
Konny Zurcher <SeaShadow@Konny.local>
2010-10-29 16:24:29 +1100

    Removed h1 tag

--- modules/rtComment/templates/createSuccess.php
+++ modules/rtComment/templates/createSuccess.php
@@ -1,10 +1,7 @@
 <?php use_helper('I18N', 'rtForm') ?>
 <?php use_stylesheets_for_form($form) ?>
 <?php use_javascripts_for_form($form) ?>
-
-<?php include_partial('rtAdmin/flashes_public') ?>
-
-<div class="rt-container rt-comment">
-  <h1><?php echo __('Comments') ?></h1>
-  <?php include_component('rtComment', 'form', array('form' => $form)) ?>
-</div>
+<?php slot('rt-title') ?>
+<?php echo __('Comments') ?>
+<?php end_slot(); ?>
+<?php include_component('rtComment', 'form', array('form' => $form)) ?>

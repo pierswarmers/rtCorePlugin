e47273d1c20f7401b91b9725842728b7bae79052
Konny Zurcher <SeaShadow@Konny.local>
2010-11-05 14:08:58 +1100

    Adjusted templates for use with boilerplate template

--- modules/rtComment/templates/createSuccess.php
+++ modules/rtComment/templates/createSuccess.php
@@ -1,7 +1,5 @@
 <?php use_helper('I18N', 'rtForm') ?>
 <?php use_stylesheets_for_form($form) ?>
 <?php use_javascripts_for_form($form) ?>
-<?php slot('rt-title') ?>
-<?php echo __('Comments') ?>
-<?php end_slot(); ?>
+<?php slot('rt-title', __('Comments')) ?>
 <?php include_component('rtComment', 'form', array('form' => $form)) ?>
--- modules/rtComment/templates/savedSuccess.php
+++ modules/rtComment/templates/savedSuccess.php
@@ -1,10 +1,8 @@
 <?php
-
 use_helper('I18N');
 $routes = $sf_context->getRouting()->getRoutes();
 $route_name = Doctrine_Inflector::tableize($model).'_show';
-
 ?>
-<h1><?php echo __('Thanks for the comment') ?></h1>
+<?php slot('rt-title', __('Thanks for the comment')) ?>
 <p><?php echo __('Your comment has been added and is awaiting approval.') ?></p>
 <p><?php echo link_to_if(isset($routes[$route_name]), __('Go back'), $route_name, $object) ?></p>

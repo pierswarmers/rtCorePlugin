5154137616515358f7ddbc31f988cdf9fa321c66
Konny <konny@wranglers.com.au>
2010-11-10 09:53:52 +1100

    Added javascript function to snippet panel template to populate the edit link to prevent cache issues

--- modules/rtSnippet/templates/_snippetPanel.php
+++ modules/rtSnippet/templates/_snippetPanel.php
@@ -7,12 +7,19 @@
       <?php foreach($snippets as $snippet): ?>
         <?php $sf_user->getAttributeHolder()->remove('referer'); ?>
         <?php echo link_to(__('Edit'), 'rtSnippetAdmin/edit?id='.$snippet->getId(), array('class' => 'rt-admin-edit-tools-trigger')) ?>
-        <a href="<?php echo url_for('rtSnippetAdmin/edit?id='.$snippet->getId()) . '?referer='.  urlencode($referer) ?>" class="rt-admin-edit-tools-trigger"><?php echo __('Edit') ?></a>
+        <a href="<?php echo url_for('rtSnippetAdmin/edit?id='.$snippet->getId()) ?>" class="rt-admin-edit-tools-trigger"><?php echo __('Edit') ?></a>
         <?php echo markdown_to_html($snippet->getContent(), $snippet); ?>
       <?php endforeach; ?>
     <?php else: ?>
-      <a href="<?php echo url_for('rtSnippetAdmin/new?collection='.$collection) . '?referer='.  urlencode($referer) ?>" class="rt-admin-edit-tools-trigger"><?php echo __('Edit') ?></a>
+      <a href="<?php echo url_for('rtSnippetAdmin/new?collection='.$collection) ?>" class="rt-admin-edit-tools-trigger"><?php echo __('Edit') ?></a>
       <?php echo isset($default) ? markdown_to_html($default) : '';  ?>
     <?php endif; ?>
   </div>
+<script type="text/javascript">
+  $(function() {
+    $("a.rt-admin-edit-tools-trigger").click(function(){
+      $("a.rt-admin-edit-tools-trigger").attr("href", $(this).attr('href') + '?referer=' + window.location);
+    })
+  });
+</script>
 <?php endif; ?>

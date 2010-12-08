1c348868535e754b348ef0697ed0eb5cbe74ccec
Piers Warmers <hello@pierswarmers.com>
2010-12-09 09:45:14 +1100

    Use new authorisation form.

--- modules/rtGuardAuth/actions/actions.class.php
+++ modules/rtGuardAuth/actions/actions.class.php
@@ -34,7 +34,7 @@ class rtGuardAuthActions extends BasesfGuardAuthActions
       return $this->redirect('@homepage');
     }
 
-    $class = sfConfig::get('app_sf_guard_plugin_signin_form', 'sfGuardFormSignin'); 
+    $class = sfConfig::get('app_sf_guard_plugin_signin_form', 'rtGuardFormSignin');
     $this->form = new $class();
 
     if ($request->isMethod('post'))

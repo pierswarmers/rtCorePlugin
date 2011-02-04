491835ef28099217611d9640abd7611ec96e2a36
Piers Warmers <hello@pierswarmers.com>
2011-02-04 14:42:34 +1100

    Corrected missing I18N helper.
    Signed-off-by: Piers Warmers <hello@pierswarmers.com>

--- modules/rtSearch/templates/_form.php
+++ modules/rtSearch/templates/_form.php
@@ -1,3 +1,4 @@
+<?php use_helper('I18N') ?>
 <form class="rt-search" action="<?php echo url_for('@rt_search') ?>" method="get">
   <p>
     <input type="text" name="q" value="<?php echo $sf_request->getParameter('q', '') ?>" placeholder="<?php echo __('Enter your search terms') ?>..." />

099be7febc9387b625b6ddbbfa8bcf449238fe77
Konny <konny@wranglers.com.au>
2011-02-09 16:26:52 +1100

    Added open graph general meta tags

--- lib/toolkit/rtResponseToolkit.class.php
+++ lib/toolkit/rtResponseToolkit.class.php
@@ -28,10 +28,28 @@ class rtResponseToolkit
   public static function setCommonMetasFromPage(rtPage $rt_page, sfUser $sf_user, sfWebResponse $sf_response)
   {
     $data = array();
-    $data['robots'] = $rt_page->getSearchable();
-    $data['keywords'] = $rt_page->getTags();
+    $data['robots'] = $rt_page->getSearchable() ? 'index, follow' : 'NONE';
+    $data['keywords'] = implode(', ', $rt_page->getTags());
     $data['title'] = $rt_page->getTitle();
     $data['description'] = $rt_page->getDescription();
+    
+    $data['og:title'] = $rt_page->getTitle();
+    $data['og:type'] = self::getTypeFromObject($rt_page);
+    $data['og:url'] = sfContext::getInstance()->getRouting()->generate('rt_shop_product_show', $rt_page, true);
+    if($rt_page->getPrimaryImage())
+    {
+      $img_path = rtAssetToolkit::getThumbnailPath($rt_page->getPrimaryImage()->getSystemPath(), array('maxHeight' => 400, 'maxWidth' => 400));
+      $data['og:image'] = rtSiteToolkit::getCurrentDomain(null, true).$img_path;
+    }
+    if(sfConfig::has('app_rt_title'))
+    {
+      $data['og:site_name'] = sfConfig::get('app_rt_title');
+    }
+    if($rt_page->getDescription() !== '')
+    {
+      $data['og:description'] = $rt_page->getDescription();
+    }
+
     self::setCommonMetas($data, $sf_response);
   }
   
@@ -44,24 +62,36 @@ class rtResponseToolkit
    */
   public static function setCommonMetas(array $data, sfWebResponse $sf_response)
   {
-    if(isset($data['searchable']))
+    foreach($data as $key => $value)
     {
-      $sf_response->addMeta('robots', $data['searchable'] ? 'index, follow' : 'NONE');
+      $sf_response->addMeta($key, $value);
+    }
+  }
+
+  /**
+   * Return open graph type based on object
+   *
+   * @param rtPage $object
+   * @return String
+   */
+  public static function getTypeFromObject(rtPage $object)
+  {
+    // Special open graph type defined in the model
+    if(method_exists($object, 'getOpenGraphType'))
+    {
+      return $object->getOpenGraphType();
     }
 
-    if(isset($data['keywords']))
-    {
-      $sf_response->addMeta('keywords', is_array($data['keywords']) ? implode(', ', $data['keywords']) : $data['keywords']);
-    }
-
-    if(isset($data['title']))
-    {
-      $sf_response->addMeta('title', $data['title']);
-    }
-
-    if(isset($data['title']))
-    {
-      $sf_response->addMeta('description', $data['description']);
+    // General open graph types
+    switch (get_class($object)) {
+      case 'rtBlogPage':
+        return 'article';
+      case 'rtWikiPage':
+        return 'article';
+      case 'rtShopProduct':
+        return 'product';
+      default:
+        return 'website';
     }
   }
 }

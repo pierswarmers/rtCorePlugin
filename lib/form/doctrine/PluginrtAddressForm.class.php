b42e224b3271b1dc9e5ba595deabcef9c2b0fba2
Piers Warmers <hello@pierswarmers.com>
2011-05-18 10:57:48 +1000

    Added new handling for simple modes of address and profile.

--- lib/form/doctrine/PluginrtAddressForm.class.php
+++ lib/form/doctrine/PluginrtAddressForm.class.php
@@ -34,30 +34,14 @@ abstract class PluginrtAddressForm extends BasertAddressForm
       unset($this['first_name'], $this['last_name']);
     }
     
-    unset($this['care_of'], $this['created_at'], $this['updated_at'], $this['model_id']);
+    unset($this['care_of'], $this['created_at'], $this['updated_at'], $this['model_id'], $this['longitude'], $this['latitude']);
 
     if (!$object)
     {
       throw new InvalidArgumentException('You must provide a parent object.');
     }
 
-    // start countries >>>
-    $c = new sfCultureInfo(sfContext::getInstance()->getUser()->getCulture());
-    $countries = $c->getCountries();
-
-    foreach($countries as $key => $value)
-    {
-      if(is_int($key))
-      {
-        unset($countries[$key]);
-      }
-    }
-
-    unset($countries['ZZ']);
-
-    $countries = array('' => '--') + $countries;
-    $this->setWidget('country', new sfWidgetFormSelect(array('choices' => $countries)));
-    // <<< end countries
+    $this->setWidget('country', new rtWidgetFormSelectCountry());
 
     $this->setWidget('state',        new rtWidgetFormSelectRegion(array('add_empty' => '--', 'country' => $this->getObject()->getCountry())));
     $this->setWidget('instructions', new sfWidgetFormInput());
@@ -66,75 +50,29 @@ abstract class PluginrtAddressForm extends BasertAddressForm
     $this->widgetSchema->moveField('country', 'before', 'state');
 
     $this->setValidators(array(
-      'id'         => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
-      'type'       => new sfValidatorChoice(array('choices' => array(0 => 'billing', 1 => 'shipping'), 'required' => false)),
-      'care_of'    => new sfValidatorString(array('max_length' => 100, 'required' => false)),
-      'address_1'  => new sfValidatorString(array('max_length' => 100, 'required' => false)),
-      'address_2'  => new sfValidatorString(array('max_length' => 100, 'required' => false)),
-      'town'       => new sfValidatorString(array('max_length' => 100, 'required' => false)),
-      'state'      => new sfValidatorString(array('max_length' => 100, 'required' => false)),
-      'postcode'   => new sfValidatorString(array('max_length' => 10, 'required' => false)),
-      'country'    => new sfValidatorString(array('max_length' => 20, 'required' => false)),
-      'model'      => new sfValidatorString(array('max_length' => 20, 'required' => false)),
-      'instructions'    => new sfValidatorString(array('max_length' => 255, 'required' => false)),
+      'id'            => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
+      'type'          => new sfValidatorChoice(array('choices' => array(0 => 'billing', 1 => 'shipping'), 'required' => false)),
+      'care_of'       => new sfValidatorString(array('max_length' => 100, 'required' => false)),
+      'address_1'     => new sfValidatorString(array('max_length' => 100, 'required' => false)),
+      'address_2'     => new sfValidatorString(array('max_length' => 100, 'required' => false)),
+      'town'          => new sfValidatorString(array('max_length' => 100, 'required' => false)),
+      'state'         => new sfValidatorString(array('max_length' => 100, 'required' => false)),
+      'postcode'      => new sfValidatorString(array('max_length' => 10, 'required' => false)),
+      'country'       => new sfValidatorString(array('max_length' => 20, 'required' => false)),
+      'model'         => new sfValidatorString(array('max_length' => 20, 'required' => false)),
+      'instructions'  => new sfValidatorString(array('max_length' => 255, 'required' => false)),
       'first_name'    => new sfValidatorString(array('max_length' => 50, 'required' => false)),
-      'last_name'    => new sfValidatorString(array('max_length' => 50, 'required' => false)),
-      'phone'        => new sfValidatorString(array('max_length' => 20, 'required' => false))
+      'last_name'     => new sfValidatorString(array('max_length' => 50, 'required' => false)),
+      'phone'         => new sfValidatorString(array('max_length' => 20, 'required' => false))
     ));
 
     $this->validatorSchema->setPostValidator(new rtAddressValidator(array('use_names' => $this->getOption('use_names', false), 'is_optional' => $is_optional)));
     $this->widgetSchema->setHelp('phone', 'Please include your area code.');
   }
 
-  /**
-   * Renders the widget schema associated with this form.
-   *
-   * @param  array  $attributes  An array of HTML attributes
-   *
-   * @return string The rendered widget schema
-   */
-  public function render($attributes = array())
-  {
-    return $this->getCountryEnhancement() . $this->getFormFieldSchema()->render($attributes);
-  }
-
   public function bind(array $taintedValues = null, array $taintedFiles = null)
   {
     parent::bind($taintedValues, $taintedFiles);
     $this->setWidget('state', new rtWidgetFormSelectRegion(array('add_empty' => '--', 'country' => $taintedValues['country'])));
-  }
-
-  public function getCountryEnhancement()
-  {
-    $form_name = $this->getName();
-
-    return "
-
-<script type=\"text/javascript\">
-  $(function() {
-    $('#". $form_name ."_country').change(function() {
-
-      var holder =  $('#". $form_name ."_state').parent();
-
-      holder.html('<span class=\"loading\">Loading states...</span>');
-      $('#". $form_name ."_state').remove();
-      $.ajax({
-        type: 'POST',
-        url: '/rtAdmin/stateInput',
-        data: ({
-          country : $(this).find('option:selected').attr('value'),
-          id      : '". $form_name ."_state',
-          name    : '". $form_name ."[state]'
-        }),
-        dataType: 'html',
-        success: function(data) {
-          holder.html(data);
-        }
-      });
-    });
-  });
-</script>
-
-";
   }
 }

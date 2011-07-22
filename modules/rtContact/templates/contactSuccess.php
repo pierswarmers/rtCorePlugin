a00b52226e86aafe6b54fca89b1d77c9328183d7
Piers Warmers <hello@pierswarmers.com>
2011-07-22 16:21:33 +1000

    Cleaned comment form in preperation for new layout changes

--- modules/rtContact/templates/contactSuccess.php
+++ modules/rtContact/templates/contactSuccess.php
@@ -1,19 +1,37 @@
-<?php use_helper('I18N', 'rtForm') ?>
-<?php use_stylesheets_for_form($form) ?>
-<?php use_javascripts_for_form($form) ?>
+<?php
 
-<?php slot('rt-title') ?>
-<?php echo __('Contact Us') ?>
-<?php end_slot(); ?>
+use_helper('I18N', 'rtForm', 'rtTemplate');
 
-<?php include_component('rtSnippet','snippetPanel', array('collection' => 'rt-contact-prefix','sf_cache_key' => 'rt-contact-prefix', 'default' => __('Please fill out your enquiry details in the form below.'))); ?>
-<form action="<?php echo url_for('rt_contact') ?>" method="post" class="rt-compact">
-  <?php echo $form->renderHiddenFields() ?>
-  <fieldset>
-  <legend><?php echo __('Your Details') ?></legend>
-    <ul class="rt-form-schema">
-      <?php echo $form; ?>
-    </ul>
-  </fieldset>
-  <p class="rt-form-tools"><button type="submit"><?php echo __('Submit Details') ?></button></p>
-</form>
+slot('rt-title', __('Contact Us'));
+
+use_stylesheets_for_form($form);
+use_javascripts_for_form($form);
+
+?>
+
+
+<div class="rt-section rt-contact">
+
+  <?php if(sfConfig::get('app_rt_templates_headers_embedded', true)): ?>
+  <div class="rt-section-header">
+    <h1><?php echo __('Contact Us') ?></h1>
+  </div>
+  <?php endif; ?>
+  
+  <div class="rt-section-content">
+
+    <?php rt_get_snippet('rt-contact-prefix', 'Feel free to send through any comments, queries or concerns. We\'ll get back to you shortly.'); ?>
+
+    <form action="<?php echo url_for('rt_contact') ?>" method="post" class="rt-compact">
+      <?php echo $form->renderHiddenFields() ?>
+      <fieldset>
+        <ul class="rt-form-schema">
+          <?php echo $form; ?>
+        </ul>
+      </fieldset>
+      <p class="rt-section-tools-submit"><button type="submit"><?php echo __('Submit your comment') ?></button></p>
+    </form>
+    
+  </div>
+
+</div>

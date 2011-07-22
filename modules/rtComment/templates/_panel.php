65f0dab13d1bd01743d1ab7251dcae7c3aa8cffe
Piers Warmers <hello@pierswarmers.com>
2011-07-22 16:21:57 +1000

    Cleaned comment form in preperation for new layout changes

--- modules/rtComment/templates/_panel.php
+++ modules/rtComment/templates/_panel.php
@@ -1,22 +1,5 @@
 <?php use_helper('I18N', 'Date', 'rtText', 'rtGravatar') ?>
 
-<style type="text/css">
-  /* Slider */
-  #rating               { margin: 0 10px; }
-  
-  /* Rating styling */
-  .rt-rating            { display: inline-block; vertical-align: text-bottom; }
-  
-  .rt-rating-text       { display: none; }
-  
-  .rt-rating-graph, .rt-rating-graph div { height: 20px; background-image: url('/rtUserPlugin/img/rating.png'); background-position: 0 0; background-repeat: repeat-x; }
-  .rt-rating-graph      { width: 100px; background-color: #CCC; }
-   .rt-rating-graph div { background-color: #000; }
-   
-   .rt-metas .rt-rating-graph, .rt-metas .rt-rating-graph div { background-position: 0 -20px; height: 10px; }
-   .rt-metas .rt-rating-graph { width: 50px; }
-</style>
-
 <?php $commenting = sfConfig::get('app_rt_comment_active', true) && sfConfig::get('app_rt_comment_active', true) ?>
 
 <?php if ($commenting): ?><div class="rt-section rt-comments"><?php endif; ?>
@@ -24,41 +7,50 @@
 <?php if(sfConfig::get('app_rt_comment_active', true) && count($comments) > 0): ?>
 
   <div class="rt-section-header">
-    <h3><?php echo count($comments) . ' ' .  __('responses to') . ' "' . $title . '"' ?>
+    <h2><?php echo count($comments) . ' ' .  __('People Have Left a Comment') ?>
     <?php if($rating_enabled): ?>
       <?php include_partial('rtComment/rating', array('rating_value' => $parent_object->getOverallRating(), 'show_items' => array('text','graph'))) ?>
     <?php endif; ?>
-    </h3>
+    </h2>
   </div>
 
-  <div class="rt-section-content">
-    <ul class="rt-comments-list">
+  <div class="rt-section-content rt-comments-list">
+    <ul>
       <?php foreach($comments as $comment): ?>
-        <li class="clearfix" id="comment-<?php echo $comment->getId() ?>">
+        <li id="rt-comment-<?php echo $comment->getId() ?>">
           <?php echo gravatar_for($comment->getAuthorEmail(), sfConfig::get('app_rt_gravatar_size_medium', 64), sfConfig::get('app_rt_gravatar_default', 'mm'), sfConfig::get('app_rt_gravatar_rating', 'g')) ?>
-          <div>
-            <cite>
-              <?php echo link_to_if(trim($comment->getAuthorWebsite()) !== '', $comment->getAuthorName(), $comment->getAuthorWebsite()) ?> 
+          <div class="rt-comment-content">
+            <cite class="rt-metas">
+              <?php echo link_to_if(trim($comment->getAuthorWebsite()) !== '', $comment->getAuthorName(), $comment->getAuthorWebsite()) ?> <?php echo __('said') ?>
+              ( <a href="#comment-<?php echo $comment->getId() ?>" title=""><?php echo format_date($comment->getCreatedAt(), 'D', $sf_user->getCulture()) ?></a>)
+              <?php if($rating_enabled): ?>
+                <span class="rating">, <?php echo __('Rating') ?>: <?php include_partial('rtComment/rating', array('rating_value' => $comment->getRating(), 'show_items' => array('graph'))) ?></span>
+              <?php endif; ?>
             </cite>
-            <small class="rt-metas">
-              <?php echo __('Left On') ?> <a href="#comment-<?php echo $comment->getId() ?>" title=""><?php echo format_date($comment->getCreatedAt(), 'D', $sf_user->getCulture()) ?></a>
-              <span class="rating"><?php if($rating_enabled): ?><?php include_partial('rtComment/rating', array('rating_value' => $comment->getRating(), 'show_items' => array('graph'))) ?><?php endif; ?></span>
-            </small>
             <?php echo markdown_to_html_safe($comment->getContent()) ?>
           </div>
         </li>
       <?php endforeach; ?>
     </ul>
-    <p class="rt-comment-tools"><a href="" target="_self"><span><?php echo __('Show more comments') ?></span><span style="display: none"><?php echo __('Show less comments') ?></span></a></p>
+    <p class="rt-comment-tools"><a href="" target="_self"><span><?php echo __('Show more comments') ?></span><span style="display: none"><?php echo __('Show fewer comments') ?></span></a></p>
   </div>
     
 <script type="text/javascript">
   $(function(){
-    $('.rt-comments .rt-comments-list li:gt(1)').hide();
+
+    // Only show the toggle button if needed.
+    if($('.rt-comments-list li').size() < 3) { $('.rt-comment-tools a').hide(); }
+
+    // Look for special cases, like when a hash link is present
+    if(location.href.indexOf("#") == -1) {
+      $('.rt-comments .rt-comments-list li:gt(1)').hide();
+    } else {
+      $('.rt-comment-tools span').toggle();
+    }
+
+    // Enable the onclick action
     $('.rt-comment-tools a').click(function(){
-      event.preventDefault();    
-      $(this).find('span').toggle();
-      $('.rt-comments .rt-comments-list li:gt(1)').toggle();
+      event.preventDefault();  $(this).find('span').toggle(); $('.rt-comments .rt-comments-list li:gt(1)').toggle();
     });
   });  
 </script>    

021bb6586d6e6e52c930c9f7c07dc9c6a2755ed5
Piers Warmers <hello@pierswarmers.com>
2011-07-22 16:21:03 +1000

    Added notice and success variations on flash display

--- modules/rtAdmin/templates/_flashes.php
+++ modules/rtAdmin/templates/_flashes.php
@@ -1,8 +1,10 @@
 <?php use_helper('I18N') ?>
 <?php if ($sf_user->hasFlash('notice')): ?>
-  <p class="success"><?php echo $sf_user->getFlash('notice') ?></p>
+  <p class="rt-flash-message notice"><?php echo $sf_user->getFlash('notice') ?></p>
+<?php elseif ($sf_user->hasFlash('success')): ?>
+  <p class="rt-flash-message success"><?php echo $sf_user->getFlash('notice') ?></p>
 <?php elseif ($sf_user->hasFlash('error')): ?>
-  <p class="error"><?php echo $sf_user->getFlash('error') ?></p>
+  <p class="rt-flash-message error"><?php echo $sf_user->getFlash('error') ?></p>
 <?php elseif ($sf_user->hasFlash('default_error')): ?>
-  <p class="error"><?php echo __('One or more errors were detected - please see below for details.') ?></p>
+  <p class="rt-flash-message error"><?php echo __('One or more errors were detected - please see below for details.') ?></p>
 <?php endif; ?>

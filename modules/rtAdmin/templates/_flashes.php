f62caa9855228b15be83b2b74293959ba52fcbed
Piers Warmers <hello@pierswarmers.com>
2011-08-22 17:05:26 +1000

    Corrected message reference for 'success' flashes.

--- modules/rtAdmin/templates/_flashes.php
+++ modules/rtAdmin/templates/_flashes.php
@@ -1,10 +1,10 @@
 <?php use_helper('I18N') ?>
 <?php if ($sf_user->hasFlash('notice')): ?>
-  <p class="rt-flash-message notice"><?php echo $sf_user->getFlash('notice') ?></p>
+  <p class="rt-flash-message notice"><?php echo html_entity_decode($sf_user->getFlash('notice')) ?></p>
 <?php elseif ($sf_user->hasFlash('success')): ?>
-  <p class="rt-flash-message success"><?php echo $sf_user->getFlash('notice') ?></p>
+  <p class="rt-flash-message success"><?php echo html_entity_decode($sf_user->getFlash('success')) ?></p>
 <?php elseif ($sf_user->hasFlash('error')): ?>
-  <p class="rt-flash-message error"><?php echo $sf_user->getFlash('error') ?></p>
+  <p class="rt-flash-message error"><?php echo html_entity_decode($sf_user->getFlash('error')) ?></p>
 <?php elseif ($sf_user->hasFlash('default_error')): ?>
   <p class="rt-flash-message error"><?php echo __('One or more errors were detected - please see below for details.') ?></p>
 <?php endif; ?>

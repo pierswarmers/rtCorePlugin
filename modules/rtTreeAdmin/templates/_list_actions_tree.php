84cf096bfce50db690264974fded67d340bf1cda
Piers Warmers <hello@pierswarmers.com>
2011-11-16 12:23:50 +1100

    Corrected bug for button status.

--- modules/rtTreeAdmin/templates/_list_actions_tree.php
+++ modules/rtTreeAdmin/templates/_list_actions_tree.php
@@ -1,10 +1,17 @@
 <ul class="rt-admin-tools">
-  <li><button disabled="disabled" type="submit" class="nodeinteraction createnode ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon"><span class="ui-button-icon-primary ui-icon ui-icon-plus"></span><span class="ui-button-text"><?php echo __('Create new item inside selected');?></span></button></li>
-  <li><button disabled="disabled" type="submit" class="nodeinteraction deletenode ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon"><span class="ui-button-icon-primary ui-icon ui-icon-trash"></span><span class="ui-button-text"><?php echo __('Delete selected item');?></span></button></li>
+  <li><button type="submit" class=" createnode ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon"><span class="ui-button-icon-primary ui-icon ui-icon-plus"></span><span class="ui-button-text"><?php echo __('Create new item inside selected');?></span></button></li>
+  <li><button type="submit" class=" deletenode ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon"><span class="ui-button-icon-primary ui-icon ui-icon-trash"></span><span class="ui-button-text"><?php echo __('Delete selected item');?></span></button></li>
 </ul>
 
-<?php echo javascript_tag();?>
+<ul class="rt-admin-tools">
+</ul>
+<script type="text/javascript">
     $(document).ready(function(){
+
+<!--$("#rtPrimaryTools .cancel").button({-->
+<!--  icons: { primary: 'ui-icon-cancel' }-->
+<!--}).click(function(){ document.location.href='--><?php //echo url_for($controller.'/index') ?><!--'; });-->
+<!---->
 
     $('.createnode').click(function(e){
         var t = $.tree.focused(); 
@@ -15,6 +22,7 @@
         else {
             alert("Select a node first");
         }
+
     });
                 
     $('.deletenode').click(function(e){
@@ -32,4 +40,4 @@
     });
 
     })
-<?php echo end_javascript_tag();?>
+</script>

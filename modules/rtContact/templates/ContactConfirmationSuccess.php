30f0adb53665c06b34e3e3c296faaaf3ed1116fb
Piers Warmers <hello@pierswarmers.com>
2012-09-13 14:34:14 +1000

    Name correction

--- modules/rtContact/templates/ContactConfirmationSuccess.php
+++
@@ -1,7 +0,0 @@
-<?php use_helper('I18N', 'rtForm', 'rtTemplate'); ?>
-
-<?php slot('rt-title') ?>
-<?php echo __('Message Sent') ?>
-<?php end_slot(); ?>
-
-<?php echo rt_get_snippet('rt-contact-confirmation-prefix', 'Thank you for your enquiry, we\'ll be in touch shortly.'); ?>

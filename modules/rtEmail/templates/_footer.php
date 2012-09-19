988457916fd037fe28dce906d5e46c048a76f6cc
Piers Warmers <hello@pierswarmers.com>
2012-09-20 09:01:04 +1000

    Footer now uses site title.

--- modules/rtEmail/templates/_footer.php
+++ modules/rtEmail/templates/_footer.php
@@ -1,4 +1,11 @@
-<?php use_helper('I18N') ?>
+<?php
+
+use_helper('I18N');
+$rt_site = rtSiteToolkit::getCurrentSite();
+
+?>
 <?php if (sfConfig::has('app_rt_email_signature_html')): ?>
 <p><?php echo sfConfig::get('app_rt_email_signature_html', '') ?></p>
+<?php elseif($rt_site): ?>
+<p>Many thanks, <?php echo $rt_site->getTitle() ?></p>
 <?php endif; ?>

a5b0d7e53adfaee033344d4b1963087bdc17d728
Piers Warmers <hello@pierswarmers.com>
2012-10-30 14:12:38 +1100

    Corrected headings.

--- modules/rtContact/templates/bookingConfirmationSuccess.php
+++ modules/rtContact/templates/bookingConfirmationSuccess.php
@@ -1,8 +1,8 @@
 <?php use_helper('I18N', 'rtForm', 'rtTemplate'); ?>
 
 <?php slot('rt-title') ?>
-<?php echo __('Message Sent') ?>
+<?php echo __('Booking Sent') ?>
 <?php end_slot(); ?>
 
 
-<?php echo rt_get_snippet('rt-contact-confirmation-prefix', 'Thank you for your enquiry, we\'ll be in touch shortly.'); ?>
+<?php echo rt_get_snippet('rt-booking-confirmation-prefix', 'Thank you for your booking, we\'ll be in touch shortly.'); ?>
--- modules/rtContact/templates/bookingSuccess.php
+++ modules/rtContact/templates/bookingSuccess.php
@@ -2,7 +2,7 @@
 
 use_helper('I18N', 'rtForm', 'rtTemplate');
 
-slot('rt-title', __('Contact Us'));
+slot('rt-title', __('Bookings'));
 
 use_stylesheets_for_form($form);
 use_javascripts_for_form($form);
@@ -14,13 +14,13 @@ use_javascripts_for_form($form);
 
   <?php if(sfConfig::get('app_rt_templates_headers_embedded', true)): ?>
   <div class="rt-section-header">
-    <h1><?php echo __('Contact Us') ?></h1>
+    <h1><?php echo __('Bookings') ?></h1>
   </div>
   <?php endif; ?>
   
   <div class="rt-section-content">
 
-    <?php rt_get_snippet('rt-contact-prefix', 'Feel free to send through any comments, queries or concerns. We\'ll get back to you shortly.'); ?>
+    <?php rt_get_snippet('rt-booking-prefix', ''); ?>
 
     <form action="<?php echo url_for('rt_booking') ?>" method="post" class="rt-compact formstyle">
       <?php echo $form->renderHiddenFields() ?>
@@ -29,7 +29,7 @@ use_javascripts_for_form($form);
           <?php echo $form; ?>
         </ul>
       </fieldset>
-      <p class="rt-section-tools-submit"><button type="submit"><?php echo __('Submit your comment') ?></button></p>
+      <p class="rt-section-tools-submit"><button type="submit"><?php echo __('Submit your booking') ?></button></p>
     </form>
     
   </div>

1d214ae630da6fb370bb585ce5333ff8accdc3ea
Piers Warmers <hello@pierswarmers.com>
2013-03-21 10:40:56 +1100

    Added moinor db update.

--- data/sql/update.sql
+++ data/sql/update.sql
@@ -2,17 +2,17 @@
 
 -- Version 0.0.1 - June 25 2010
 
-ALTER TABLE `rt_site` ADD COLUMN `reference_key` VARCHAR(255);
-ALTER TABLE `rt_site` DROP COLUMN `template_dir`;
-ALTER TABLE `rt_site` DROP COLUMN `is_primary`;
+ALTER TABLE rt_site ADD COLUMN reference_key VARCHAR(255);
+ALTER TABLE rt_site DROP COLUMN template_dir;
+ALTER TABLE rt_site DROP COLUMN is_primary;
 
 -- Version 0.0.2 - June 29 2010
 
-ALTER TABLE `rt_address` ADD COLUMN `instructions` TEXT;
-ALTER TABLE `rt_address` ADD COLUMN `first_name` VARCHAR(100);
-ALTER TABLE `rt_address` ADD COLUMN `last_name` VARCHAR(100);
+ALTER TABLE rt_address ADD COLUMN instructions TEXT;
+ALTER TABLE rt_address ADD COLUMN first_name VARCHAR(100);
+ALTER TABLE rt_address ADD COLUMN last_name VARCHAR(100);
 
-ALTER TABLE `rt_shop_order` ADD COLUMN `voucher_code` VARCHAR(100);
+ALTER TABLE rt_shop_order ADD COLUMN voucher_code VARCHAR(100);
 
 -- Version 0.0.3 - March 15 2012
 
@@ -44,3 +44,5 @@ ALTER TABLE rt_site CHANGE meta_title meta_title_suffix VARCHAR(255);
 ALTER TABLE rt_site ADD COLUMN meta_keywords_suffix VARCHAR(255);
 
 ALTER TABLE rt_site CHANGE meta_keywords_suffix meta_keyword_suffix VARCHAR(255);
+
+ALTER TABLE rt_site ADD COLUMN type VARCHAR(255);

660129c4b03038209cabb2d194c50fd7b0da6467
Piers Warmers <hello@pierswarmers.com>
2013-08-13 11:25:19 +1000

    Added new primary image call on snippet component.

--- modules/rtSnippet/templates/_snippetPrimaryImage.php
+++ modules/rtSnippet/templates/_snippetPrimaryImage.php
@@ -9,13 +9,6 @@ $snippets = $sf_data->getRaw('snippets');
 
 /** @var $snippets Doctrine_Collection */
 if ($snippets && count($snippets) && $snippets->get(0)->getPrimaryImage()) {
-//    echo get_class();
-//    echo rt_thumbnail($snippets->get(0), $width, $height);
-
-
-//    echo $snippets->get(0)->getPrimaryImage()->getWebPath();
-
     echo rtAssetToolkit::getThumbnailPath($snippets->get(0)->getPrimaryImage()->getSystemPath(), array('maxHeight' => $width, 'maxWidth' => $height));
 } else {
-    echo 'asd';
 }

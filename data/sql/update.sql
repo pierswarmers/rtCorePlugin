87e39870681442d5c2622cffc4390a48f16ffda6
Piers Warmers <hello@pierswarmers.com>
2014-07-26 13:06:40 +1000

    Upgraded the update sql

--- data/sql/update.sql
+++ data/sql/update.sql
@@ -35,6 +35,18 @@ ALTER TABLE rt_site ADD COLUMN category VARCHAR(255);
 ALTER TABLE rt_site ADD COLUMN created_at DATETIME NOT NULL;
 ALTER TABLE rt_site ADD COLUMN updated_at DATETIME NOT NULL;
 
+ALTER TABLE rt_site ADD COLUMN public_url VARCHAR(255);
+ALTER TABLE rt_site ADD COLUMN position INT DEFAULT '1';
+ALTER TABLE rt_site ADD COLUMN target_link VARCHAR(255);
+ALTER TABLE rt_site ADD COLUMN content_summery TEXT;
+ALTER TABLE rt_site ADD COLUMN email_contact_address VARCHAR(255);
+ALTER TABLE rt_site ADD COLUMN email_contact_response TEXT;
+ALTER TABLE rt_site ADD COLUMN html_suffix TEXT;
+ALTER TABLE rt_site ADD COLUMN html_snippet_suffix TEXT;
+ALTER TABLE rt_site ADD COLUMN email_booking_address VARCHAR(255);
+ALTER TABLE rt_site ADD COLUMN email_booking_response TEXT;
+ALTER TABLE rt_site ADD COLUMN google_plus_url VARCHAR(255);
+
 -- Version 0.0.4 - March 19 2013
 
 ALTER TABLE rt_site ADD COLUMN sub_title VARCHAR(255);
@@ -66,3 +78,46 @@ ALTER TABLE rt_snippet_version ADD COLUMN uri VARCHAR(255);
 
 ALTER TABLE rt_snippet ADD COLUMN uri_target VARCHAR(255);
 ALTER TABLE rt_snippet_version ADD COLUMN uri_target VARCHAR(255);
+
+-- Version 0.0.8 - July 23 2014
+
+ALTER TABLE rt_site ADD COLUMN public_url VARCHAR(255);
+ALTER TABLE rt_site ADD COLUMN position INT DEFAULT '1';
+ALTER TABLE rt_site ADD COLUMN target_link VARCHAR(255);
+ALTER TABLE rt_site ADD COLUMN content_summery TEXT;
+ALTER TABLE rt_site ADD COLUMN email_contact_address VARCHAR(255);
+ALTER TABLE rt_site ADD COLUMN email_contact_response TEXT;
+ALTER TABLE rt_site ADD COLUMN html_suffix TEXT;
+ALTER TABLE rt_site ADD COLUMN html_snippet_suffix TEXT;
+ALTER TABLE rt_site ADD COLUMN email_booking_address VARCHAR(255);
+ALTER TABLE rt_site ADD COLUMN email_booking_response TEXT;
+ALTER TABLE rt_site ADD COLUMN google_plus_url VARCHAR(255);
+
+ALTER TABLE rt_page ADD COLUMN mode VARCHAR(255) DEFAULT 'standard';
+ALTER TABLE rt_page_version ADD COLUMN mode VARCHAR(255) DEFAULT 'standard';
+ALTER TABLE rt_site_page ADD COLUMN mode VARCHAR(255) DEFAULT 'standard';
+ALTER TABLE rt_site_page_version ADD COLUMN mode VARCHAR(255) DEFAULT 'standard';
+ALTER TABLE rt_blog_page ADD COLUMN mode VARCHAR(255) DEFAULT 'standard';
+ALTER TABLE rt_blog_page_version ADD COLUMN mode VARCHAR(255) DEFAULT 'standard';
+
+
+ALTER TABLE rt_wiki_page ADD COLUMN mode VARCHAR(255) DEFAULT 'standard';
+ALTER TABLE rt_wiki_page_version ADD COLUMN mode VARCHAR(255) DEFAULT 'standard';
+
+ALTER TABLE rt_snippet ADD COLUMN mode VARCHAR(255) DEFAULT 'standard';
+ALTER TABLE rt_snippet_version ADD COLUMN mode VARCHAR(255) DEFAULT 'standard';
+
+
+ALTER TABLE rt_page ADD COLUMN title_head VARCHAR(255);
+ALTER TABLE rt_page_version ADD COLUMN title_head VARCHAR(255);
+ALTER TABLE rt_site_page ADD COLUMN title_head VARCHAR(255);
+ALTER TABLE rt_site_page_version ADD COLUMN title_head VARCHAR(255);
+ALTER TABLE rt_blog_page ADD COLUMN title_head VARCHAR(255);
+ALTER TABLE rt_blog_page_version ADD COLUMN title_head VARCHAR(255);
+
+
+ALTER TABLE rt_wiki_page ADD COLUMN title_head VARCHAR(255);
+ALTER TABLE rt_wiki_page_version ADD COLUMN title_head VARCHAR(255);
+
+ALTER TABLE rt_asset ADD COLUMN target_url VARCHAR(255);
+
